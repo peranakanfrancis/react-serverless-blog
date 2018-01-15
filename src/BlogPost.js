c793ce2aaf3f2276dc3b0e8e074052312e965af8
peranakanfrancis <francis.tch@gmail.com>
2018-01-16 07:17:55 +0800

    first commit

---
+++ src/BlogPost.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import Butter from 'buttercms'
+import { Helmet } from "react-helmet";
+
+const butter = Butter('fdcafeb0b43e4eb97e49e50f38fcf2a731bca3ff');
+
+class BlogPost extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loaded: false
+    };
+  }
+
+  componentWillMount() {
+    let slug = this.props.params.slug;
+
+    butter.post.retrieve(slug).then((resp) => {
+      this.setState({
+        loaded: true,
+        post: resp.data.data
+      })
+    });
+  }
+
+  render() {
+    if (this.state.loaded) {
+      const post = this.state.post;
+
+      return (
+        <div>
+          <Helmet>
+            <title>{post.seo_title}</title>
+            <meta name="description" content={post.meta_description} />
+            <meta name="og:image" content={post.featured_image} />
+          </Helmet>
+
+          <h1>{post.title}</h1>
+          <div dangerouslySetInnerHTML={{__html: post.body}} />
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          Loading...
+        </div>
+      );
+    }
+  }
+}
+
+export default BlogPost;
